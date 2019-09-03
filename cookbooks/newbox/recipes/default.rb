8515cab0aacd0a7d78269105d839442e6c41a85e
Matt Lewis <matt@mplewis.com>
2019-09-02 19:25:52 -0600

    feat: go, gopath, krypton, hub

--- cookbooks/newbox/recipes/default.rb
+++ cookbooks/newbox/recipes/default.rb
@@ -1,13 +1,21 @@
 USER = 'mplewis'
 APT_PACKAGES = %w(
-  inetutils-tools
   dos2unix
-  zip
+  golang-go
+  inetutils-tools
   unzip
+  zip
   zsh
 )
 
 HOME = node['etc']['passwd'][USER]['dir']
+
+# https://github.com/golang/go/wiki/Ubuntu
+execute 'add_golang_backports_repo' do
+  # https://askubuntu.com/a/148954
+  not_if 'grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/* | grep longsleep-ubuntu-golang-backports'
+  command 'sudo add-apt-repository ppa:longsleep/golang-backports'
+end
 
 APT_PACKAGES.each do |pkg|
   apt_package pkg
@@ -16,6 +24,11 @@ end
 git_user USER do
   full_name 'Matt Lewis'
   email 'matt@mplewis.com'
+end
+
+execute 'fix_gopath' do
+  not_if { ENV['GOPATH'] }
+  command 'export GOPATH=~/.gopath'
 end
 
 execute 'install_oh-my-zsh' do
@@ -35,5 +48,24 @@ bash 'install_scm_breeze' do
     git clone git://github.com/scmbreeze/scm_breeze.git ~/.scm_breeze
     ~/.scm_breeze/install.sh
     source ~/.zshrc
+  CMDS
+end
+
+execute 'install_krypton' do
+  not_if 'which kr'
+  command 'curl https://krypt.co/kr | sh'
+end
+
+bash 'install_hub' do
+  not_if 'which hub'
+  code <<~CMDS
+    git clone \
+      --config transfer.fsckobjects=false \
+      --config receive.fsckobjects=false \
+      --config fetch.fsckobjects=false \
+      https://github.com/github/hub.git /tmp/hub
+    cd /tmp/hub
+    make install prefix=/usr/local
+    rm -rf /tmp/hub
   CMDS
 end
