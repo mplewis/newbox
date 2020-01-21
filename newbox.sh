2533ecaab491a10bbf6d5d36be6de5be34d3683e
Matt Lewis <matt@mplewis.com>
2020-01-21 11:45:29 -0700

    fix: install berks on first time

--- newbox.sh
+++ newbox.sh
@@ -3,6 +3,7 @@
 if ! [ -x "$(command -v chef)" ]; then
   echo 'Installing Chef...'
   curl -L https://www.opscode.com/chef/install.sh | sudo bash
+  sudo gem install berkshelf
 
   (
     cd cookbooks/newbox || return
